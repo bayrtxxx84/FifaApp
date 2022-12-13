b4344e20e7acd97bf0ac967ffe14ca604d4a9cc3
Bayron Torres <torres.bayron@gmail.com>
2022-12-13 18:09:42 -0500

    Second commit. Add cards component and change to the login layout

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.test
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.test.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initClass()
+
     }
+
+
+    override fun onPause() {
+        super.onPause()
+        println("PAUSANDO APP")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("REANUDANDO APP")
+    }
+
+    private fun initClass() {
+        val txt = binding.txtPass
+        binding.buttonLogin.setOnClickListener {
+            Snackbar.make(txt, txt.text.toString(), Snackbar.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+
 }
