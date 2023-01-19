36488109f47f970ab686bc8497d65639f1ef20fb
Bayron Torres <torres.bayron@gmail.com>
2023-01-18 22:39:40 -0500

    RecyclerView y SearchView

--- app/src/main/java/com/example/test/ui/adapters/UserAdapter.kt
+++ app/src/main/java/com/example/test/ui/adapters/UserAdapter.kt
@@ -1,23 +1,41 @@
 package com.example.test.ui.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.test.R
+import com.example.test.databinding.CountriesRvBinding
 import com.example.test.model.entities.api.Countries
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
 
-class UserAdapter(val dataList: List<Countries>) :
+class UserAdapter() :
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
-    class UserViewHolder() {
+    var dataList: List<Countries> = emptyList()
+
+    class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private var binding: CountriesRvBinding = CountriesRvBinding.bind(view)
+
+        fun render(item: Countries) {
+            binding.txtV1.text = item.alternateName
+            binding.txtV2.text = item.country
+            Picasso.get().load("https://countryflagsapi.com/png/" + item.alternateName)
+                .into(binding.countryImage);
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        return UserViewHolder(inflater.inflate(R.layout.countries_rv, parent, false))
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.render(dataList[position])
     }
 
     override fun getItemCount(): Int = dataList.size
--- app/src/main/java/com/example/test/ui/fragments/FragmentFrancia.kt
+++ app/src/main/java/com/example/test/ui/fragments/FragmentFrancia.kt
@@ -1,26 +1,89 @@
 package com.example.test.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.test.databinding.FragmentFranciaBinding
+import com.example.test.model.entities.api.Countries
+import com.example.test.ui.adapters.UserAdapter
+import com.example.test.userCase.teams.TeamsUC
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class FragmentFrancia : Fragment() {
 
-private lateinit var binding : FragmentFranciaBinding
+    private lateinit var binding: FragmentFranciaBinding
+    private val listCountries = ArrayList<Countries>()
+    private val adapter = UserAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentFranciaBinding.inflate(inflater , container, false)
+    ): View {
+        binding = FragmentFranciaBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        loadCountries()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun loadCountries() {
+
+        lifecycleScope.launch {
+            val c = TeamsUC().getInfoTeam("bra")
+            if (c != null) {
+                listCountries.add(c)
+            }
+            val c1 = TeamsUC().getInfoTeam("ecu")
+            if (c1 != null) {
+                listCountries.add(c1)
+            }
+            val c2 = TeamsUC().getInfoTeam("ger")
+            if (c2 != null) {
+                listCountries.add(c2)
+            }
+
+            adapter.dataList = listCountries
+            binding.listCountriesRV.adapter = adapter
+            binding.listCountriesRV.layoutManager = LinearLayoutManager(
+                activity?.baseContext,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+        }
 
 
+        binding.searchCountriesRV.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val filter = listCountries.filter {
+                    it.alternateName.lowercase() == query?.lowercase()
+                }
+                adapter.dataList = filter
+                adapter.notifyDataSetChanged()
+                return true
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+
+        binding.searchCountriesRV.setOnCloseListener {
+            adapter.dataList = listCountries
+            adapter.notifyDataSetChanged()
+            true
+        }
+    }
 }
